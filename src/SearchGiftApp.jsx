8e5931b3f424f095e2513746583aa0b98a453670
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2024-06-19 16:44:10 +0200

    Inicio del proyecto

---
+++ src/SearchGiftApp.jsx
@@ -0,0 +1,9 @@
+const SearchGiftApp = () => {
+  return (
+    <div>
+      <h1>Buscador de Gifs</h1>
+    </div>
+  )
+}
+
+export default SearchGiftApp
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SearchGiftApp from './SearchGiftApp.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SearchGiftApp />
+  </React.StrictMode>,
+)
